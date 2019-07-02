ecab4dec2d3bded37181da5414f80e7c53f5f429
guoyi <395380680@qq.com>
2019-07-02 11:17:31 +0800

    生成配置文件。并测试

---
+++ HelloWord.js
@@ -0,0 +1,7 @@
+"use strict";
+var a = 'hello world';
+console.log(a);
+var num = 18;
+console.log(num);
+var b = undefined;
+console.log(b);
---
+++ HelloWord.ts
@@ -0,0 +1,6 @@
+var a:string = 'hello world'
+console.log(a);
+var num:number = 18;
+console.log(num);
+let b:undefined = undefined;
+console.log(b);
